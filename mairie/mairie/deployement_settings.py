1cecce069e3c7de4ebe14d1940f0262a59405acd
Arthur Talom <talarthur04@gmail.com>
2026-01-07 14:53:22 +0100

    cors origin added

--- mairie/mairie/deployement_settings.py
+++ mairie/mairie/deployement_settings.py
@@ -26,7 +26,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-#CORS_ALLOWED_ORIGINS = os.environ.get('CORS_ALLOWED_ORIGINS')
+CORS_ALLOWED_ORIGINS = ['https://mairie-dev-frontend.onrender.com']
 
 STORAGES = {
     "default": {
